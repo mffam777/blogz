e8a62e292c84c731ddd4bfcf1f9023a1c590dd8a
AM <mffam777@gmail.com>
2019-01-05 20:37:52 -0500

    Created SQLAlchemy connection

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
-from flask import Flask, Request, redirect, render_template
+from flask import Flask, request, redirect, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:root@localhost:3306/build-a-blog'
+app.config['SQLALCHEMY_ECHO'] = True
+
+db = SQLAlchemy(app)
+
+class Task(db.Model):
+
+    id = db.Column(db.Interger, primary_key=True)
+    name = db.Column(db.String(120))
+
+    #constructor
+    def __init__(self, name):
+        self.name = name
 
 task = []
 
