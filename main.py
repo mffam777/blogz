e444ccc7037e0d50c152fb6310d2b95306b54288
AM <mffam777@gmail.com>
2019-01-06 19:52:12 -0500

    add html coding to blog.html and work error message in main.py

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 from flask import Flask, redirect, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 
+
 app = Flask(__name__)
-db = SQLAlchemy(app)
+
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:root@localhost:80/build-a-blog'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:root@localhost :80/build-a-blog'
+
+db = SQLAlchemy(app)
 app.config['SQLALCHEMY_ECHO'] = True
 
 
-class Task(db.Model):
+class Blog(db.Model):
 
     id = db.Column(db.Interger, primary_key=True)
     name = db.Column(db.String(120))
@@ -22,16 +25,16 @@ class Task(db.Model):
     #def __repr__(self):
         #return '<User %r>' % self.name
 
-task = []
+blogs = []
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
 
     if request.method == 'POST':
-        task = request.form('task')
-        task.append(task)
+        blog = request.form('blog')
+        blogs.append(blog)
 
-    return render_template('todos.html', title="Build a Blog", task=task)
+    return render_template('blog.html', title="Build a Blog", blogs=blogs)
 
 # shield app
 if __name__ == '__main__':
