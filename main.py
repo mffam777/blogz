f27b86239dc27f9ae5975273090cc14c57a44704
AM <mffam777@gmail.com>
2019-01-10 13:36:59 -0500

    added top admin information

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
+# School: Launch Code
+# Class: LC101 Ft. Meade, MD F18
+# Student: Alberto Morales
+# Instructor: Patrick Kozub
+# TA: Jesse Shaw
+# TA: Benjamin Muyinda
+# Assignment: build-a-blog
+# Due date: January 02, 2019 @ 21:00
+# Location: Anne Arundel Community College; Mall Campus
+# Git Hub Repository: https://github.com/mffam777/-build-a-blog.git
+
+
 from flask import Flask, render_template, request, redirect, flash, url_for
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
