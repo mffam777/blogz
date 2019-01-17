ae5469d10426a8bbec41d2bb9982a4ec1b9f0fdf
AM <mffam777@gmail.com>
2019-01-16 19:09:35 -0500

    added hash

--- main.py
+++ main.py
@@ -12,59 +12,58 @@
 
 from flask import Flask, render_template, request, redirect, flash, url_for, session
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, BooleanField, TextAreaField, Form, TextField
-from wtforms.validators import DataRequired, ValidationError
-import secrets
-
-
-app = Flask(__name__)
-app.secret_key = 'secret'
-
+#from datetime import datetime
+#from flask_wtf import FlaskForm
+#from wtforms import StringField, SubmitField, BooleanField, TextAreaField, Form, TextField
+#from wtforms.validators import DataRequired, ValidationError, Email
+#import secrets
+from hashutils import make_pw_hash, check_pw_hash, make_salt
 
 # Note: the connection string after :// contains the following info:
 # user:password@server:portNumber/databaseName
 
+app = Flask(__name__)
+app.config["DEBUG"] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:otro@localhost:3306/blogz'
 app.config['SQLALCHEMY_ECHO'] = True
-
-app.config["DEBUG"] = True
 db = SQLAlchemy(app)
+app.secret_key = 'secret'
+
 
 class Blog(db.Model):
     
     id = db.Column(db.Integer, primary_key=True)
+    #name = db.Column(db.String(120))
     title = db.Column(db.String(120))
+    # completed = db.Column(db.Boolean)
     post = db.Column(db.Text, nullable=False)
     owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    #date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 
-    def __init__(self, title, post, owner):
+    def __init__(self, title, owner):
         self.title = title
         self.post = False        
-        self.post = Blog
+        #self.post = Blog
         self.owner = owner
 
 class User(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True)
-    password = db.Column(db.String(120))
+    pw_hash = db.Column(db.String(120))
     blogs = db.relationship('Blog', backref='owner')
 
     def __init__(self, email, password):
         self.email = email
-        self.password = password
+        self.pw_hash = make_pw_hash(password, salt=None)
 
 @app.before_request
 #@app.route('/', methods=['POST', 'GET'])
 def require_login():
     
     allowed_routes = ['login', 'signup']
-
     if request.endpoint not in allowed_routes and 'email' not in session:
-        return redirect('/signup')
+        return redirect('/login')
 
 # initial page
 @app.route('/login', methods=['POST', 'GET'])
@@ -73,8 +72,7 @@ def login():
         email = request.form['email']
         password = request.form['password']
         user = User.query.filter_by(email=email).first()
-    
-        if user and user.password == password:
+        if user and check_pw_hash(password, user.pw_hash):
             session['email'] = email
             flash("Logged in")
             # verify index.html
@@ -96,24 +94,24 @@ def signup():
 
         # TODO - validate user's data
 
-    existing_user = User.query.filter_by(email=email).first()
-    if not existing_user:
+        existing_user = User.query.filter_by(email=email).first()
+        if not existing_user:
             new_user = User(email, password)
             db.session.add(new_user)
             db.session.commit()
             session['email'] = email
             return redirect('/')
-    else:
+        else:
             # TODO - user better response messaging
             return "<h1>Duplicate user</h1>"
 
-            return render_template('signup.html')
+    return render_template('signup.html')
 
 @app.route('/logout')
 def logout():
     del session['email']
 
-    return redirect('/', "<h1>You are signed out</h1>")
+    return redirect('/')
 
 
 @app.route('/', methods=['POST', 'GET'])
@@ -122,7 +120,7 @@ def index():
 
     owner = User.query.filter_by(email=session['email']).first()
 
-    form = Blog()
+    #form = Blog()
     #if request.method == "GET":
         #blogs = Blog.query.all()
         #print(blogs[1].id)
@@ -141,12 +139,12 @@ def index():
         #print("-------------------")
         #print(request.form)
         print("-------------------")
-        #blog_name = request.form['blog']
-        blog_id = request.form['blog']
+        blog_name = request.form['blog']
+        #blog_id = request.form['blog']
         #blog_title = request.form['btitle']
         #blog_post = request.form['new_blog']
-        #new_blog = Blog(blog_name, owner)
-        new_blog = Blog(blog_id, owner)
+        new_blog = Blog(blog_name, owner)
+        #new_blog = Blog(blog_id, owner)
         db.session.add(new_blog)
         db.session.commit()
 
